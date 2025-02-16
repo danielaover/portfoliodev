573d86c419255a3db1810d79ebdc8393b7ca7ed2
danielaover <danielarosa.dev@outlook.com>
2025-02-16 19:11:52 -0300

    primeiro commit

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let navbar = document.querySelector('.navbar');
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
-}
+};
 
 
 let sections = document.querySelectorAll('section');
